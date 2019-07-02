ee0cb8499bf17d7f545739762a7a9a9ffa33f536
Dan Ryan <dan@danryan.co>
2019-07-02 17:28:24 -0400

    move plugin back to its own location
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/airflow_salesforce_plugin/__init__.py
+++ src/airflow_salesforce_plugin/__init__.py
@@ -1,35 +1,4 @@
 # Copyright (c) 2019 Dan Ryan
 # MIT License <https://opensource.org/licenses/mit>
-from typing import List
-from airflow.plugins_manager import AirflowPlugin
-from airflow.utils.log.logging_mixin import LoggingMixin
-
-from .hooks import SalesforceHook
-from .operators import (
-    SalesforceAttachmentToS3Operator,
-    SalesforceToFileOperator,
-    SalesforceToS3Operator,
-)
-
-
-class SalesforcePlugin(AirflowPlugin):
-    """Apache Airflow Salesforce Plugin."""
-
-    name = "salesforce_plugin"
-    hooks: List = [SalesforceHook]
-    operators: List = [
-        SalesforceToFileOperator,
-        SalesforceAttachmentToS3Operator,
-        SalesforceToS3Operator,
-    ]
-    executors: List = []
-    macros: List = []
-    admin_views: List = []
-    flask_blueprints: List = []
-    menu_links: List = []
-
-
-
-__all__ = ["SalesforcePlugin"]
-__all__ = ["SalesforcePlugin", "__version__"]
-__version__ = "0.0.1"
+__all__ = ["__version__"]
+__version__ = "0.0.2"
---
+++ src/airflow_salesforce_plugin/plugin.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from airflow.plugins_manager import AirflowPlugin
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+from .hooks import SalesforceHook
+from .operators import (
+    SalesforceAttachmentToS3Operator,
+    SalesforceToFileOperator,
+    SalesforceToS3Operator,
+)
+
+
+class SalesforcePlugin(AirflowPlugin):
+    """Apache Airflow Salesforce Plugin."""
+
+    name = "salesforce_plugin"
+    hooks: List = [SalesforceHook]
+    operators: List = [
+        SalesforceToFileOperator,
+        SalesforceAttachmentToS3Operator,
+        SalesforceToS3Operator,
+    ]
+    executors: List = []
+    macros: List = []
+    admin_views: List = []
+    flask_blueprints: List = []
+    menu_links: List = []
+
+
+__all__ = ["SalesforcePlugin"]
